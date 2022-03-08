0ba31e6dc3511cc6b73ad049712c249873f34836
Joel Nitta <joelnitta@gmail.com>
2022-03-08 18:07:55 +0900

    Assign title based on output type

--- R/render.R
+++ R/render.R
@@ -6,7 +6,7 @@ rmarkdown::render(
   knit_root_dir = here::here(),
   output_format = "bookdown::word_document2",
   output_file = here::here("results/gameto-review-ms.docx"),
-  params = list(doc_type = "docx")
+  params = list(doc_type = "docx", title = "")
 )
 
 # Render to pdf (pre-print)
@@ -15,7 +15,9 @@ rmarkdown::render(
   knit_root_dir = here::here(),
   output_format = "bookdown::pdf_document2",
   output_file = here::here("results/gameto-review-ms.pdf"),
-  params = list(doc_type = "pdf")
+  params = list(
+    doc_type = "pdf",
+    title = "Identifying cryptic fern gametophytes using DNA barcoding: A review")
 )
 
 # Compress pdf
